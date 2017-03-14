3e84af6aad0d2bde93b8dd60469c76299d934349
Panderium <alexis.fougerouse2@etu.univ-lyon1.fr>
2017-03-14 16:39:24 +0100

    pièce

--- src/Piece.java
+++ src/Piece.java
@@ -1,8 +1,20 @@
-/**
- * Created by p1609137 on 14/02/2017.
- */
-public class Piece {
-    public void Piece(){
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class Piece {
+    private Color color;
+    private Position p ;
+    private boolean[][] shape;
+
+    public Piece(Color color, Position p, boolean[][] shape) {
+        this.color = color;
+        this.shape = shape;
+        this.p = p;
     }
+
+    public boolean isEmpty() {
+        return shape.isEmpty();
+    }
+
 }
--- src/PieceFactory.java
+++ src/PieceFactory.java
@@ -1,12 +1,57 @@
-
+import java.util.HashMap;
 
 public class PieceFactory {
 
-    public PieceFactory(){
+
+    public Piece create(char shape, Color color) {
+        Piece newPiece;
+
+        switch (shape) {
+            case 'Z':
+                newPiece = new Piece();
+                break;
+            case 'O':
+                newPiece = new Piece();
+                break;
+            case 'I':
+                newPiece = new Piece();
+                break;
+            case 'S':
+                newPiece = new Piece();
+                break;
+            case 'L':
+                newPiece = new Piece();
+                break;
+            case 'J':
+                newPiece = new Piece();
+                break;
+            case 'T':
+                newPiece = new Piece();
+                break;
+        }
+
+        return newPiece;
+
+
+
+        switch (gameName){
+            case 'Tetris':
+                newPiece = createTetris(color);
+                break;
+
+            case 'blokus':
+                createBlokus(gameName, color)
+                break;
+
+            default:
+                break;
+        }
 
     }
 
-    public Piece create(){
-        return new Piece();
+    private createTetris(Color color) {
+        Piece
+
     }
+
 }
